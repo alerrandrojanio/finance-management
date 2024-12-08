a5bee64292ce1c36cccee77317fe29f585a7a244
Alerrandro Janio <alerrandro.janio07@gmail.com>
2024-12-08 11:22:09 -0300

    feat: add transaction type badge

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,3 +1,33 @@
-export default function Transactions() {
-  return <div>Transactions</div>;
+import { auth } from "@clerk/nextjs/server";
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
+import { redirect } from "next/navigation";
+
+export default async function Transactions() {
+  const { userId } = await auth();
+
+  if (!userId) redirect("/login");
+
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  return (
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
 }
