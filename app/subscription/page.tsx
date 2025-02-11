750bcdddeca45d9ee9489d06524d62e19ad3bfc2
Alerrandro Janio <alerrandro.janio07@gmail.com>
2025-02-11 13:11:49 -0300

    feat: add navbar

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,3 +1,5 @@
+import { Navbar } from "../_components/navbar";
+
 export default function Subscription() {
-  return <div>Subscription</div>;
+  return <Navbar />;
 }
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -4,6 +4,7 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import { redirect } from "next/navigation";
 import { AddTransactionButton } from "../_components/add-transaction-button";
+import { Navbar } from "../_components/navbar";
 
 export default async function Transactions() {
   const { userId } = await auth();
@@ -20,13 +21,17 @@ export default async function Transactions() {
   });
 
   return (
-    <div className="space-y-6 p-6">
-      <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl font-bold">Transações</h1>
-        <AddTransactionButton />
-      </div>
+    <>
+      <Navbar />
 
-      <DataTable columns={transactionColumns} data={transactions} />
-    </div>
+      <div className="space-y-6 p-6">
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddTransactionButton />
+        </div>
+
+        <DataTable columns={transactionColumns} data={transactions} />
+      </div>
+    </>
   );
 }
